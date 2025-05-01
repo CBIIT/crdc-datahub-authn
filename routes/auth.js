345a15f93fdb5384fb06cfd2fb3afdac63849153
knockknockyoo <iheaven0129@gmail.com>
2025-05-01 10:11:26 -0400

    Log error message.

--- routes/auth.js
+++ routes/auth.js
@@ -23,6 +23,7 @@ router.post('/login', async function (req, res) {
     try {
         const reqIDP = config.getIdpOrDefault(req.body['IDP']);
         const { name, lastName, tokens, email, idp } = await idpClient.login(req.body['code'], reqIDP, config.getUrlOrDefault(reqIDP, req.body['redirectUri']));
+        console.log(`email:${email} IDP:${idp} is logging in`);
         if (!await userService.isEmailAndIDPLoginPermitted(email, idp)) {
             throw { statusCode: 403, message: ERROR.INACTIVE_USER };
         }
@@ -35,7 +36,9 @@ router.post('/login', async function (req, res) {
         req.session.tokens = tokens;
         res.json({name, email, "timeout": config.session_timeout / 1000});
         await logCollection.insert(LoginEvent.create(email, idp));
+        console.log(`email:${email} IDP:${idp} logged in successfully`);
     } catch (e) {
+        console.error(e);
         if (e.code && parseInt(e.code)) {
             res.status(e.code);
         } else if (e.statusCode && parseInt(e.statusCode)) {
@@ -53,10 +56,12 @@ router.post('/logout', async function (req, res, next) {
         const idp = config.getIdpOrDefault(req.body['IDP']);
         const userInfo = req?.session?.userInfo;
         if (userInfo?.email && userInfo?.IDP) await logCollection.insert(LogoutEvent.create(userInfo.email, userInfo.IDP));
+        console.log(`email:${userInfo?.email} IDP:${userInfo?.IDP} is logging out`);
         await idpClient.logout(idp, req.session.tokens);
+        console.log(`email:${userInfo?.email} IDP:${userInfo?.IDP} logged out successfully`);
         return logout(req, res);
     } catch (e) {
-        console.log(e);
+        console.error(e);
         res.status(500).json({errors: e});
     }
 });
@@ -72,7 +77,7 @@ router.post('/authenticated', async function (req, res, next) {
             return res.status(200).send({status: false});
         }
     } catch (e) {
-        console.log(e);
+        console.error(e);
         res.status(500).json({errors: e});
     }
 });
